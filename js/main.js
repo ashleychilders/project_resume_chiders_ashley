ddd340b57d2a9d95ceab6d70daa0f17a9240df92
Ashley Childers <ashley.childers@gmail.com>
2016-04-11 22:17:57 -0400

    Updated backstretch file and add to js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready( function() {
+
+    $.backstretch( [
+    "img/renwickblack.jpg",
+    "img/renwickstack.jpg",
+    "img/renwickrainbow.jpg"
+], { duration: 3000, fade: 750 } );
+
+} );
