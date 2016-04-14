344311dce8a791d3b695abf4e9aa7d91baaab893
Ashley Childers <ashley.childers@gmail.com>
2016-04-14 17:12:45 -0400

    Add smooth scroll to sections

--- js/main.js
+++ js/main.js
@@ -9,4 +9,17 @@ $( document ).ready( function() {
             duration: 3900, fade: 850 }
         );
 
+        $( '.anchor-scroll' ).anchorScroll(
+            {
+            scrollSpeed: 800,
+            offsetTop: 0,
+            onScroll: function() {
+
+         // callback on scroll start
+            },
+            scrollEnd: function() {
+
+         // callback on scroll end
+            }
+        } );
 } );
